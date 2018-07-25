6f6c41663731d69e15c31acbd434d1dfca0dd7ca
Brecht Dierckx <brecht.dierckx@restore.eu>
2018-07-25 09:39:45 +0200

    skinner-1: refine FifoFixedSizeMemory documentation and tests

--- src/skinner/memories.py
+++ src/skinner/memories.py
@@ -1,24 +1,23 @@
 import random
 
-from typing import List
-
 
 class FifoFixedSizeMemory(object):
 
     def __init__(self, capacity: int):
+        """ A first in, first out queue with a fixed maximum capacity """
         self.capacity = capacity
         self.memory = []
         self.position = 0
 
     def commit(self, *args):
-        """ Commit args to memory """
+        """ Commit args to memory. """
         if len(self.memory) < self.capacity:
             self.memory.append(None)
         self.memory[self.position] = args
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size: int):
-        """ Randomly sample a number of elements from memory. """
+        """ Randomly sample a number of elements from memory without replacement."""
         return random.sample(self.memory, batch_size)
 
     def __len__(self):
--- tests/test_memories.py
+++ tests/test_memories.py
@@ -12,11 +12,11 @@ class TestReplayMemory(unittest.TestCase):
             empty_mem.sample(1)
 
     def test_sample_without_repetition(self):
-        memory = filled_random_memory(5)
+        memory = random_memory(5)
         for _ in range(40):
             self.assertEqual(set(memory.sample(5)), set(memory.sample(5)))
 
-    def test_push_increases_memory_size_until_capacity(self):
+    def test_commit_only_increases_memory_size_up_to_capacity(self):
         mem = FifoFixedSizeMemory(5)
         for i in range(1,6):
             mem.commit(i)
@@ -24,7 +24,7 @@ class TestReplayMemory(unittest.TestCase):
         mem.commit(6)
         self.assertEqual(len(mem), 5)
 
-    def test_push_replaces_oldest_element_first(self):
+    def test_commit_replaces_oldest_element_first(self):
         mem = FifoFixedSizeMemory(5)
         for i in range(1, 6):
             mem.commit(i)
@@ -32,8 +32,13 @@ class TestReplayMemory(unittest.TestCase):
         mem.commit(6)
         self.assertTrue(1 not in set(mem.sample(5)))
 
+    def test_sample_over_capacity_raises_value_error(self):
+        memory = random_memory(3)
+        with self.assertRaises(ValueError):
+            memory.sample(4)
 
-def filled_random_memory(size):
+
+def random_memory(size):
     mem = FifoFixedSizeMemory(size)
     for _ in range(size):
         mem.commit(random.random())
